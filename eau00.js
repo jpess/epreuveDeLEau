3f1a77af594da0142ffbbbcf6dbfb635d69b3552
jpess <julien.pession@gmail.com>
2022-02-27 21:30:43 +0800

    template done

---
+++ eau00.js
@@ -0,0 +1,29 @@
+/**
+* Combinaisons de 3 chiffres
+*/
+
+/*
+* Créez un programme qui affiche toutes les différentes combinaisons possibles
+* de trois chiffres dans l’ordre croissant, dans l’ordre croissant.
+* La répétition est volontaire.
+*/
+
+/********************
+*     Functions     *
+********************/
+
+/********************
+*   Error Handling  *
+********************/
+
+/********************
+*      Parsing      *
+********************/
+
+/********************
+*        Main       *
+********************/
+
+/********************
+*       Result      *
+********************/
