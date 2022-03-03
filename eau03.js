60539f5d595eecf8192f464e7d9f2c4e124f1a0b
jpess <julien.pession@gmail.com>
2022-03-03 23:09:53 +0800

    script complete

--- eau03.js
+++ eau03.js
@@ -1,28 +1,84 @@
 /**
-* 
+* Suite de Fibonacci
 */
 
 /*
+* Créez un programme qui affiche le N-ème élément de la célèbre suite de Fibonacci. (0, 1, 1, 2) étant le début de la suite et le premier élément étant à l’index 0.
 *
+* Exemples d’utilisation :
+* $> python exo.py 3
+* 2
+* $>
+*
+* Afficher -1 si le paramètre est négatif ou mauvais.
 */
 
 /********************
 *     Functions     *
 ********************/
 
+/**
+* Recursive function that return the Fibonacci element at the index passed in argument
+*/
+function getFibonacciElementByIndex(index){
+  switch (index) {
+    case 0:
+      return 0;
+      break;
+    case 1:
+      return 1;
+      break;
+    default:
+      return getFibonacciElementByIndex(index - 1) + getFibonacciElementByIndex(index - 2);
+      break;
+  }
+}
+
 /********************
 *   Error Handling  *
 ********************/
+const errorNegative = new Error("Argument must be a positive Integer.");
+const errorNotAnInteger = new Error("Argument is not an Integer.");
+const errorWrongArguments = new Error("Wrong arguments.");
+const error = new Error("-1");
+
+function isArgumentNegative(argument){
+  return(argument<0);
+}
+
+function isArgumentNotAnInteger(argument){
+  return(argument % 1 !== 0);
+}
+
+function isThereOneAndOnlyOneArgument(args){
+  return(args.length == 1);
+}
 
 /********************
 *      Parsing      *
 ********************/
-// const x = process.argv[x_index]
+const args = process.argv.slice(2);
 
 /********************
 *        Main       *
 ********************/
+let element = -1;
+if(isThereOneAndOnlyOneArgument(args)){
+  const indexElement = args[0];
+  if(!isArgumentNotAnInteger(indexElement)){
+      if(!isArgumentNegative(indexElement)){
+        element = getFibonacciElementByIndex(indexElement);
+      } else {
+        console.log(errorNegative.message);
+      }
+  } else {
+    console.log(errorNotAnInteger.message);
+  }
+} else {
+  console.log(errorWrongArguments.message);
+}
 
 /********************
 *       Result      *
 ********************/
+console.log(element);
