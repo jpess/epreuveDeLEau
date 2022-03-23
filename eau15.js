9836af491af08afb0f23fe5c7da996c005b15052
jpess <julien.pession@gmail.com>
2022-03-23 21:03:22 +0800

    complete

---
+++ eau15.js
@@ -0,0 +1,42 @@
+/**
+* Eau : ok
+*/
+
+/*
+* Créez un programme qui célèbre votre victoire.
+*
+* Exemples d’utilisation :
+* $> ruby exo.rb
+* J’ai terminé l’Épreuve de l’Eau et c’était [].
+* $>
+* 
+* Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+*/
+
+/********************
+*     Functions     *
+********************/
+
+/********************
+*   Error Handling  *
+********************/
+
+/********************
+*      Parsing      *
+********************/
+
+/********************
+*        Main       *
+********************/
+const result = `J’ai terminé l’Épreuve de l’Eau et c’était limpide !`;
+
+/********************
+*       Result      *
+********************/
+console.log(result);
+
+// Difficultees rencontrees
+
+// Beaucoup de recherche dans la doc pour trouver les bonnes methodes a utiliser
+// Quelques confusion sur la place de certains blocs de codes, dans Functions ou Error, etc.
+// Besoin d'enrichir ma vision sur la compisition du code pour le rendre plus claire, plus lisibles, plus logiques?
